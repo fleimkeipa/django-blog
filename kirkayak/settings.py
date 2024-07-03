4338daa4d3de2e32c2e18106c115fa449a59d0fb
fleimkeipa <adem_sahin12@hotmail.com>
2024-07-03 06:32:36 +0300

    Add packages requirements txt

--- kirkayak/settings.py
+++ kirkayak/settings.py
@@ -39,9 +39,7 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "rest_framework",
-    "rest_framework.authtoken",
     "rest_framework_simplejwt",
-    "django_extensions",
     "django_filters",
     "blog",
 ]
