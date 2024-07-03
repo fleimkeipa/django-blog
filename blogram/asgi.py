dca5b4125dcacd561ac91474f0a903776f0be8b8
fleimkeipa <adem_sahin12@hotmail.com>
2024-07-03 14:46:16 +0300

    Change names

--- blogram/asgi.py
+++ blogram/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for kirkayak project.
+ASGI config for blogram project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'kirkayak.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'blogram.settings')
 
 application = get_asgi_application()
--- blogram/wsgi.py
+++ blogram/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for kirkayak project.
+WSGI config for blogram project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'kirkayak.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'blogram.settings')
 
 application = get_wsgi_application()
